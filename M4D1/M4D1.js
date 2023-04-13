a4e9a2d33b51e4503a9d49f2ce694320708f2436
KayeF97 <118546292+KayeF97@users.noreply.github.com>
2023-04-13 16:35:26 +0100

    Add files via upload

---
+++ M4D1/M4D1.js
@@ -0,0 +1,87 @@
+//Create a function to calculate the sum of the two given integers. If the two values are the same, return triple their sum.
+
+/*const theSumOf = function (a,b){
+
+    if (a === b){
+        console.log((a+b)*3);
+    }else{
+        console.log(a+b)
+    }
+}
+
+theSumOf(2,2)*/
+
+//Create a function to check two given integers. Return true if one of them is 50 or if their sum is 50.
+
+/*const fiddyTruth = function(c,d){
+    if(c === 50 || d === 50){
+      console.log("true");
+    } else if (c+d === 50){
+        console.log("true");
+    } else {
+        console.log("nah");
+    }
+}
+
+fiddyTruth(48,2)*/
+
+//Create a function to remove a character at a specified position from a given string: pass the position and the string as parameters, return the new string.
+
+/*let theWord = "";
+let thePosition 
+let theFinalPosition
+
+const removeLetter = function(theWord,thePosition,theFinalPosition){
+   console.log(theWord.substr(thePosition, theFinalPosition));
+}
+
+removeLetter("mississippi" ,2,8)*/
+
+//Create a function to find and return the largest of three given integers.
+
+/*const theLargestOf = function (e,f,g){
+    if(e > f && e > g){
+        console.log(e);
+    } else if (f > e && f > g){
+        console.log(f);
+    } else {
+        console.log(g);
+    }
+}
+
+theLargestOf(4,10,12)*/
+
+//Create a function to check if two numbers are in the range 40-60 or 70-100. Return true if they do, return false if one (or both) don’t.
+
+const inTheRange = function ( h, i){
+
+    
+
+}
+inTheRange(61,72)
+
+//Create a function to create a new string composed of a specified number of copies of a given string. Pass the string and the number of copies as parameters.
+//Create a function to display the city name if the string begins with “Los” or “New”. Pass the city name as a parameter. Return false if they start with a different string.
+//Create a function to calculate and return the sum of all elements from an array with 3 elements. Pass the array as a parameter.
+//Create a function to test if an array of length 2 contains 1 OR 3. Return true is it does, false if it doesn’t.
+//Create a function to test if an array of length 2 DOES NOT contain 1 or 3. Return true if it doesn’t, false if it does.
+//Create a function to find the longest string from a given array of strings. Pass the array as parameter and return the longest string.
+//Create a function to find the types of a given angle:
+//Acute angle ⇒ between 0 and 90 degrees. Return acute.
+//Right angle ⇒ 90 degree. Return right
+//Obtuse angle ⇒ between 90 and 180. Return obtuse
+//Straight angle ⇒ 180 degrees. Return straight
+//Create a function to find and return the index of the greatest element of a given array of integers that you passed as a parameter
+//Create a function to find and return the largest even number from an array of integers that is passed a parameter.
+//Create a function to check from two given integers (passed as parameters) if one is positive and the other is negative. Return true if that’s the case, return false if it’s not.
+//Create a function to create and return a new string where the first 3 characters are in lower case and the others are in upper case. If the string’s length is less than 3, convert the whole string into uppercase. Pass the original string as a parameter.
+//Create a function to calculate the sum of two integers (passed as parameters). If the sum is in the 50-80 range, return 65, otherwise, return 80.
+//Create a function to convert a number (passed as a parameter) into a string, basing yourself on this example:
+//The number has 3 as a factor ⇒ return Diego
+//The number has 5 as a factor ⇒ return Riccardo
+//The number has 7 as a factor ⇒ return Stefano
+//If the number does not have 3,5, or 7, return the original number.
+//⚠️ The factor is an integer which evenly divides a number without leaving a remainder.
+//One number can have more than one factor, in that case you should return both names.
+//Ex: 15 has both 3 and 5 has factors: the function will return DiegoRiccardo
+//Create a function that that takes a phrase as a parameter and returns its acronym. Ex. British Broadcasting Corporation returns BBC
